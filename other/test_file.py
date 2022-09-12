b4e32dbe5948f8c274f9ef96a716b4c698437e97
Børge Bjørnstadjordet <borgetull@gmail.com>
2022-09-12 03:12:36 +0200

    ndarray 2
    2 forslag til implementasjon
    Begge er ikke ferdige

--- other/test_file.py
+++ other/test_file.py
@@ -1,20 +1,31 @@
-
 # Begynnelse av n-dimensional array
 
-def make_array(shape):
-    data = False
+# one way of creating nd-array
+def n_array_one(shape, values):
+    data = 0
     while len(shape) > 0:
         data = [data for x in range(shape[-1])]
         shape.pop()
     return data
 
 
+# another way
+def n_array_two(shape, values):
+    empty_list = 1
+    for n in shape:
+        empty_list = [empty_list] * n
+    return empty_list
+
+
 def main():
     shape = (2, 2, 2)
     values = (1, 2, 3, 4)
 
-    array = make_array(list(shape))
-    print(array)
+    arr1 = n_array_one(list(shape), list(values))
+    print(arr1)
+
+    arr2 = n_array_two(list(shape), list(values))
+    print(arr2)
 
 
 main()
